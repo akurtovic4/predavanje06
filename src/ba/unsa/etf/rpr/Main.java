8bb82bd7edb13893322bc4187a0be45f283c4fda
akurtovic4 <akurtovic4@etf.unsa.ba>
2021-10-18 16:57:37 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,53 @@
+package ba.unsa.etf.rpr;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+//var samo za lokalne promjenljive
+//losa praksa koristiti kombinovane operatore
+//System.exit(0) prekida program
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+          /*
+           try {
+                Scanner in = new Scanner(System.in);
+                int x = in.nextInt();
+                var ime = "Amina";
+                System.out.println( "Vrijednost je : " + x );
+                System.out.println( "Moje ime je " + ime );
+                System.out.println( "2 na 3 je: ");
+                System.out.println(Math.pow(2,3));
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.println("Niste unijeli broj");
+            }
+
+
+
+
+        int x=234234;
+        long y=x;
+        System.out.println("Vrijednost je= " +y);
+        //iz inta ne moze u short direktno pa makar i moglo stati u short
+
+        int z=2389797;
+        short v=(short)z;
+        System.out.println("Vrijednost je: " +v);
+        final double PI=3.12;
+        // PI=PI+1 NIJE LEGALNO
+
+         */
+
+
+
+
+
+
+
+
+    }
+}
